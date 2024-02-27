7d4fa73d2a829b9f7c49b3f1a9adad0edd9c1d8a
Diego Robles Rojas <125101098+Castle373@users.noreply.github.com>
2024-02-27 01:17:20 -0700

    server terminado
    besos
    
    Co-Authored-By: froggo <95252663+Sp00kyDucklyn@users.noreply.github.com>

--- Server/src/server/HiloCliente.java
+++ Server/src/server/HiloCliente.java
@@ -12,10 +12,13 @@ import java.io.InterruptedIOException;
 import java.io.OutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -29,30 +32,30 @@ public class HiloCliente extends Thread {
     private static final int ECHOMAX = 255;
     private DatagramSocket socket;
     private DatagramPacket packetRecibir;
+    ArrayList<ArrayList> listaArchivos = new ArrayList<>();
 
-    public HiloCliente(DatagramSocket socket, DatagramPacket packetRecibir) {
+    public HiloCliente(DatagramSocket socket, DatagramPacket packetRecibir, ArrayList<ArrayList> lista) {
         this.socket = socket;
         this.packetRecibir = packetRecibir;
+        this.listaArchivos = lista;
     }
 
     @Override
     public void run() {
-
         DatagramPacket packetEnviar = new DatagramPacket(new byte[255], 255, packetRecibir.getAddress(), packetRecibir.getPort());
-        String palabra = new String(packetRecibir.getData());
+//        String palabra = new String(packetRecibir.getData());
+        String palabra = "s";
         String palabraC = "LISTA";
-        System.out.println("ola");
+        System.out.println(palabra);
         if (palabra.contains(palabraC)) {
             try {
                 File archivo = new File("./archivos");
-                String[] listaArchivos = archivo.list();
+                String[] archivosStrings = archivo.list();
                 String cadenaArchivos = "";
 
-                for (String listaArchivo : listaArchivos) {
-
+                for (String listaArchivo : archivosStrings) {
                     cadenaArchivos += (listaArchivo);
                     cadenaArchivos += " ";
-
                 }
 
                 packetEnviar.setData(cadenaArchivos.getBytes());
@@ -64,39 +67,82 @@ public class HiloCliente extends Thread {
                 Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
             }
         } else {
-           try {
-//                socket.receive(packetRecibir);
 
-                String fileName = new String(packetRecibir.getData()).trim();
-                System.out.println("enviando :" + fileName);
-                
-                DatagramPacket recieveArchive;
-                
-               
-                
-                try (FileInputStream path = new FileInputStream("./archivos/" + fileName)) {
+            byte[] bytesRecibidos = packetRecibir.getData();
 
-                    // Lee el contenido actual del archivo (si existe)
-                    byte[] packet = new byte[1024];
+            byte[] bytesString = new byte[bytesRecibidos.length - Integer.BYTES];
+            System.arraycopy(bytesRecibidos, 0, bytesString, 0, bytesString.length);
 
-                    while (true) {
-                        
-                        if (!(path.read(packet) != -1)) {
-                            break;
-                        }
-                        System.out.println(packetRecibir.getAddress().toString()+" "+packetRecibir.getPort());
-                        recieveArchive = new DatagramPacket(packet, packet.length,packetRecibir.getAddress(),packetRecibir.getPort());
-                        socket.send(recieveArchive);
+            byte[] bytesInt = new byte[Integer.BYTES];
+            System.arraycopy(bytesRecibidos, bytesString.length, bytesInt, 0, bytesInt.length);
+
+            String fileName = new String(bytesString).trim();
+            ByteBuffer intBuffer = ByteBuffer.wrap(bytesInt);
+            System.out.println(fileName);
+            int numeroEntero = intBuffer.getInt();
+            if (numeroEntero != 0) {
+                System.out.println("pidio indivual");
+                System.out.println("numero "+numeroEntero);
+                File archivos = new File("./archivos");
+                int i = 0;
+                for (String listaArchivo : archivos.list()) {
+                    if (fileName.contains(listaArchivo)) {
+                        break;
                     }
-
-                    path.close();
+                    i++;
                 }
+                ArrayList<byte[]> paquetes = listaArchivos.get(i);
+                byte[] paquete = paquetes.get(numeroEntero);
+                DatagramPacket recieveArchive;
+                try {
+                        byte[] fileData = Arrays.copyOfRange(paquete, 0, 1016);
+                        byte[] etiquetas = Arrays.copyOfRange(paquete, 1016, 1024);
+                        // Convertir los últimos 4 bytes a dos números short
+                        ByteBuffer buffer2 = ByteBuffer.wrap(etiquetas);
+                        int numeroPaquete = buffer2.getInt();
+                        int totalDePaquetes = buffer2.getInt();
+                        // Mostrar los números short
+                        System.out.println("Número de paquete: " + numeroPaquete);
+                        System.out.println("Total de paquetes: " + totalDePaquetes);
+                        System.out.println("");
+                        recieveArchive = new DatagramPacket(paquete, paquete.length, packetRecibir.getAddress(), packetRecibir.getPort());
+                        socket.send(recieveArchive);
+//                }
+                    } catch (IOException ex) {
+                        Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+            } else {
 
-            } catch (IOException ex) {
-                Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
+                File archivos = new File("./archivos");
+                int i = 0;
+                for (String listaArchivo : archivos.list()) {
+                    if (fileName.contains(listaArchivo)) {
+                        break;
+                    }
+                    i++;
+                }
+                DatagramPacket recieveArchive;
+                ArrayList<byte[]> paquetes = listaArchivos.get(i);
+                for (byte[] paquete : paquetes) {
+                    try {
+                        byte[] fileData = Arrays.copyOfRange(paquete, 0, 1016);
+                        byte[] etiquetas = Arrays.copyOfRange(paquete, 1016, 1024);
+                        // Convertir los últimos 4 bytes a dos números short
+                        ByteBuffer buffer2 = ByteBuffer.wrap(etiquetas);
+                        int numeroPaquete = buffer2.getInt();
+                        int totalDePaquetes = buffer2.getInt();
+                        // Mostrar los números short
+                        System.out.println("Número de paquete: " + numeroPaquete);
+                        System.out.println("Total de paquetes: " + totalDePaquetes);
+                        System.out.println("");
+                        recieveArchive = new DatagramPacket(paquete, paquete.length, packetRecibir.getAddress(), packetRecibir.getPort());
+                        socket.send(recieveArchive);
+//                }
+                    } catch (IOException ex) {
+                        Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                }
             }
-
-        }
 
 //        while (true) {
 //            try {
@@ -132,6 +178,7 @@ public class HiloCliente extends Thread {
 //            }
 //
 //        }
-    }
+        }
 
+    }
 }
--- Server/src/server/Server.java
+++ Server/src/server/Server.java
@@ -4,11 +4,14 @@
  */
 package server;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -25,8 +28,50 @@ public class Server {
      * @param args the command line arguments
      */
     private static final int ECHOMAX = 255;
+    private static final ArrayList<ArrayList> listaArchivos = new ArrayList<>();
+    
+    private static void GenerarPaquetes() {
+        File archivos = new File("./archivos");
+        String[] archi = archivos.list();
+        for (String listaArchivo : archi) {
 
+            try (FileInputStream path = new FileInputStream("./archivos/" + listaArchivo)) {
+
+                ArrayList<byte[]> paquetes = new ArrayList<>();
+                int i = 1;
+                long tamanoArchivo = path.available();
+                int totalDePaquetes = (int) Math.ceil((double) tamanoArchivo / 1016);
+                while (true) {
+                    byte[] packet = new byte[1016];
+                    byte[] packetNum = new byte[4];
+                    byte[] packetMax = new byte[4];
+                    byte[] packetFull = new byte[1024];
+                    if (!(path.read(packet) != -1)) {
+                        break;
+                    }
+                    System.out.println(i);
+                    System.out.println(totalDePaquetes);
+                    packetNum = ByteBuffer.allocate(4).putInt(i).array();
+                    packetMax = ByteBuffer.allocate(4).putInt(totalDePaquetes).array();
+
+                    System.arraycopy(packet, 0, packetFull, 0, packet.length);
+                    System.arraycopy(packetNum, 0, packetFull, packet.length, packetNum.length);
+                    System.arraycopy(packetMax, 0, packetFull, packet.length + packetNum.length, packetMax.length);
+                    paquetes.add(packetFull);
+                    i++;
+                }
+                listaArchivos.add(paquetes);
+                path.close();
+            } catch (IOException ex) {
+                Logger.getLogger(HiloCliente.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
     public static void main(String[] args) {
+        System.out.println("Encendiendo server");
+        GenerarPaquetes();
+        System.out.println("Server Encendido");
         ArrayList<String> puertos = new ArrayList<>();
         Executor service = Executors.newCachedThreadPool();
         
@@ -39,18 +84,18 @@ public class Server {
 
         while (true) {
             try {
-                DatagramPacket packet = new DatagramPacket(new byte[ECHOMAX], ECHOMAX);
+                DatagramPacket packet = new DatagramPacket(new byte[1024], 1024);
                 
                 clientSocket.receive(packet);
                 
                 String destino = "";
                 destino+=(packet.getAddress().toString());
                 destino+=(String.valueOf(packet.getPort()));
-              
+                System.out.println(destino);
                 if (!puertos.contains(destino)) {
-//                    puertos.add(destino);
-                    service.execute(new HiloCliente(clientSocket,packet));;
                     System.out.println("Cliente nuevo");
+                    service.execute(new HiloCliente(clientSocket,packet,listaArchivos));;
+                    
                 }else{
                     System.out.println("pidio algo raro");
                 }
